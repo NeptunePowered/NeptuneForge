3ec1d0809f896caf63ed0fb3b7ab010501743eb5
Jamie Mansfield <malgm@lapis.blue>
2015-02-17 14:35:17 +0000

    Update package name

---
+++ src/main/java/uk/jamierocks/arno/ArnoCoremod.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Arno, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Jamie Mansfield <https://github.com/jamierocks>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package uk.jamierocks.arno;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+
+import java.util.Map;
+
+public class ArnoCoremod implements IFMLLoadingPlugin {
+    public ArnoCoremod() {
+        MixinBootstrap.init();
+        MixinEnvironment.getCurrentEnvironment().addConfiguration("mixins.arno.json");
+    }
+    
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[] {
+                MixinBootstrap.TRANSFORMER_CLASS
+        };
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return "uk.jamierocks.arno.ArnoMod";
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return "uk.jamierocks.arno.asm.transformers.ArnoAccessTransformer";
+    }
+}
